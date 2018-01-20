8e1fb83b013a90c4b251547810610969dac983c3
Maksim Kostromin <daggerok@gmail.com>
2018-01-20 09:10:51 +0200

    add same behavior, but on other route

--- gateway/src/main/java/daggerok/config/GatewayConfig.java
+++ gateway/src/main/java/daggerok/config/GatewayConfig.java
@@ -1,6 +1,7 @@
 package daggerok.config;
 
 import daggerok.GatewayApplication;
+import org.springframework.cloud.gateway.config.GatewayAutoConfiguration;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +9,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = GatewayApplication.class)
+@ComponentScan(basePackageClasses = {
+    GatewayApplication.class,
+    GatewayAutoConfiguration.class,
+})
 public class GatewayConfig {
 
   /**
@@ -21,10 +25,15 @@ public class GatewayConfig {
     return routeLocatorBuilder
         .routes()
 
-        .route(r -> r.path("/get")
+        .route(r -> r.path("/")
                      .addRequestHeader("X-Spring-Cloud-Gateway", "Is-F*cking-Awesome!")
-                     .uri("http://httpbin.org:80/get")
-        )
+                     .uri("http://httpbin.org:80/get"))
+
+        /* absolutely same behavior as prev */
+
+        .route(r -> r.path("/get")
+                     .addRequestHeader("X-Spring-Cloud-Gateway", "Hell-Yeah!")
+                     .uri("http://httpbin.org:80")) // but not .uri("http://httpbin.org:80/"))
 
         .build()
         ;
