4679d1f0e8b9527a62d36b061ebd19bc70b6b252
Maksim Kostromin <daggerok@gmail.com>
2018-01-20 09:29:44 +0200

    add host rewrite predicate

--- gateway/src/main/java/daggerok/config/GatewayConfig.java
+++ gateway/src/main/java/daggerok/config/GatewayConfig.java
@@ -25,18 +25,26 @@ public class GatewayConfig {
     return routeLocatorBuilder
         .routes()
 
+        /* http://127.0.0.1:8080/ -> http://httpbin.org:80/get */
         .route(r -> r.path("/")
                      .addRequestHeader("X-Spring-Cloud-Gateway", "Is-F*cking-Awesome!")
                      .uri("http://httpbin.org:80/get"))
 
         /* absolutely same behavior as prev */
-
+        /* http://127.0.0.1:8080/get -> http://httpbin.org:80/get */
         .route(r -> r.path("/get")
                      .addRequestHeader("X-Spring-Cloud-Gateway", "Hell-Yeah!")
                      .uri("http://httpbin.org:80")) // but not .uri("http://httpbin.org:80/"))
 
+        /* http://127.0.0.1:8080/get Host:my.fake.host -> http://httpbin.org:80/get */
         .route(r -> r.host("my.**.host")
                      .addRequestHeader("Ololo", "Trololo")
+                     .uri("http://httpbin.org:80"))
+
+        /* http://127.0.0.1:8080/local/get Host:boo.rewrite.org -> http://httpbin.org:80/get */
+        .route(r -> r.host("*.rewrite.org")
+                     .rewritePath("/local/(?<segment>.*)", "/${segment}")
+                     .addRequestHeader("Boo", "O.o")
                      .uri("http://httpbin.org:80"))
 
         .build()
